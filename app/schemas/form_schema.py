95696a8d9b1eeb8112dc11c5d4367ab156745070
sachinmunasinghe <sachinravisankha072@gmail.com>
2024-06-16 14:51:26 +0530

    added form validations

--- app/schemas/form_schema.py
+++ app/schemas/form_schema.py
@@ -1,17 +1,20 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, constr
 
 
 class FormSchema(BaseModel):
     id: Optional[str] = None
-    first_name: str
-    last_name: str
-    email: str
-    country: str
-    phone_number: str
-    skills: list[str]
-    experience: str
-    compensation: str
-    linkedin_url: Optional[str] = None
+    first_name: constr(min_length=1, max_length=60)
+    last_name: constr(min_length=1, max_length=60)
+    email: EmailStr
+    country: constr(min_length=1, max_length=60)
+    phone_number: constr(pattern=r'^\d{10}$')
+    skills: list[constr(min_length=1, max_length=30)]
+    experience: constr(min_length=1, max_length=20)
+    compensation: constr(min_length=1, max_length=30)
+    linkedin_url: (
+        Optional)[constr(min_length=1,
+                         max_length=100,
+                         pattern=r'^(https?://)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(\.[a-zA-Z]{2,})?(/[^\s]*)?$')] = None
     timestamp: Optional[int] = None
